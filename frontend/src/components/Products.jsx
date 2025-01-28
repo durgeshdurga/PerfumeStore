6e832788b5a35bf261d5c1963f4dfbc782cdd43a
Durgesh Kumar Durga <122013890+durgeshdurga@users.noreply.github.com>
2025-01-28 16:25:48 +0530

    Update Products.jsx

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -7,7 +7,7 @@ const Products = () => {
   const [val, setVal] = useState([]);
   useEffect( () =>{
     const fun = async () => {
-      const response = await axios.get("http://localhost:8000");
+      const response = await axios.get("https://perfumestore-backend.onrender.com");
       setVal(response.data);
     };
     fun();
